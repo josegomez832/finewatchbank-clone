e171daa0ee969f8a9f8100f721c9a3231fa2045d
josegomez832 <gomez.jose853@gmail.com>
2020-09-30 23:13:27 -0500

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from './firebase'
+
+  const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCORR_b0RNAIrJvEr7ew2XXcfujjguf-Gw",
+    authDomain: "finewatchbank.firebaseapp.com",
+    databaseURL: "https://finewatchbank.firebaseio.com",
+    projectId: "finewatchbank",
+    storageBucket: "finewatchbank.appspot.com",
+    messagingSenderId: "946636636188",
+    appId: "1:946636636188:web:5ce58fe63cc4aee3e9dfd9",
+    measurementId: "G-EEJY059Q47"
+  });
+
+  const db = firebaseApp.firestore();
+  //see https://youtu.be/B6ay3jAZN5o?t=6628
+  //const auth = firebase.auth();
+
+ export { db };
